f597597e735f38cd0e0bef69002bb2c214024881
chaitanyazode <chaitanyazode13@gmail.com>
2026-08-24 17:45:38 +0530

    update

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -23,10 +23,9 @@ class DataTransformation:
         self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
-        '''
-        This function si responsible for data trnasformation
         
-        '''
+        # This function si responsible for data trnasformation
+        
         try:
             numerical_columns = ["writing_score", "reading_score"]
             categorical_columns = [

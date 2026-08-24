8698fa01def9559fd7174e96eece79a010118779
chaitanyazode <chaitanyazode13@gmail.com>
2026-08-24 17:46:31 +0530

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from typing import List
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
 
-    # this function will return the list of requirements
+    #  this function will return the list of requirements
     
     requirements=[]
     with open(file_path) as file_obj:
@@ -23,5 +23,4 @@ author='Krish',
 author_email='krishnaik06@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
-
 )

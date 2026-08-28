0711631fc65c82e72d22ea08e531761700402795
chaitanyazode <chaitanyazode13@gmail.com>
2026-08-29 00:04:53 +0530

    logging

--- src/logger.py
+++ src/logger.py
@@ -1,5 +1,6 @@
 import logging
 import os
+
 from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
@@ -12,6 +13,4 @@ logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-
-
 )
